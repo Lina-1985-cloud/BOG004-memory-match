65ebd6495f8a1eb2e2064b2e6ed0e4eb0160498b
Lina Marcela <linavilla.2@gmail.com>
2022-02-14 11:27:33 -0500

    funcionalidad/animación/tarjetas

--- src/main.js
+++ src/main.js
@@ -2,3 +2,32 @@ import {generarTablero}from './components/App.js';
 
 document.getElementById('root').appendChild(generarTablero());
 
+// Cambio pantalla
+
+document.getElementById('root').style.display= 'none';
+const play= document.getElementById('play');
+    play.addEventListener('click', ()=>{
+        document.querySelector('div').style.display= 'none';
+        const section = document.getElementById('root').style.display= 'block';
+} )
+
+
+// función click a las tarjetas
+
+
+const tarjetas = document.querySelectorAll('.tarjeta')
+let tajetaVolteada = false;
+let primeraTarjeta, segundaTarjeta;
+
+function voltearTarjeta(){
+    this.classList.toggle('voltear');
+    // if (!tajetaVolteada) {
+    //     tajetaVolteada = true;
+    //     primeraTarjeta = this;
+
+    // }
+    console.log('click');
+    console.log(this);
+}
+
+tarjetas  .forEach (tarjeta => tarjeta.addEventListener('click', voltearTarjeta));
