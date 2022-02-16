8c4737dc564b82fcbfe5f43133916e160206baba
AnamariaMC <anymedinac14@gmail.com>
2022-02-16 16:41:13 -0500

    corrección/en/los/clicks/de/las/tarjetas

--- src/main.js
+++ src/main.js
@@ -12,35 +12,52 @@ play.addEventListener("click", () => {
 
 // función click a las tarjetas
 var tarjetaVolteada = false;
+var bloquearTablero = false;
 var primeraTarjeta, segundaTarjeta;
+
 const tarjetas = document.querySelectorAll(".tarjeta");
 function voltearTarjeta() {
+    if (bloquearTablero) return;
     this.classList.add("voltear");
     if (!tarjetaVolteada) {
         tarjetaVolteada = true;
         primeraTarjeta = this;
-    } else {
+
+        return;
+    } 
+    // } else { Segundo click
         tarjetaVolteada = false;
         segundaTarjeta = this;
-        console.log({ primeraTarjeta, segundaTarjeta });
-        console.log(primeraTarjeta.dataset.pokemon);
-        console.log(segundaTarjeta.dataset.pokemon);
-
-        if (primeraTarjeta.dataset.pokemon === segundaTarjeta.dataset.pokemon){
-            primeraTarjeta.removeEventListener('click' , voltearTarjeta);
-            segundaTarjeta.removeEventListener('click' , voltearTarjeta);
-        } else {
-            setTimeout(() => {
-                primeraTarjeta.classList.remove('voltear');
-                segundaTarjeta.classList.remove('voltear');
-            }, 1500 );
-            
-        }
-
-        console.log("la función se ejecuto")
-
-    }
+    //     // console.log({ primeraTarjeta, segundaTarjeta });
+    //     // console.log(primeraTarjeta.dataset.pokemon);
+    //     // console.log(segundaTarjeta.dataset.pokemon);
+         verParejas();          
+   
 }
+/* función para chequiar las parejas
+El operador ternario (?) nos permite remplazar un if y else validando las condiones true o false
+y ejecutando las expresiones*/ 
+function verParejas(){
+    let sonPareja = primeraTarjeta.dataset.pokemon === segundaTarjeta.dataset.pokemon
+    sonPareja ? desabilitarTarjetas(): desplegarTarjetas();
+}
+
+/* función para desabilitar las tarjetas */
+function desabilitarTarjetas (){
+    primeraTarjeta.removeEventListener('click' , voltearTarjeta);
+    segundaTarjeta.removeEventListener('click' , voltearTarjeta);
+}
+/*función para desplegar las tarjetas*/
+function desplegarTarjetas (){
+    bloquearTablero = true;
+    setTimeout(() => {
+        primeraTarjeta.classList.remove('voltear');
+        segundaTarjeta.classList.remove('voltear');
+    bloquearTablero = false    
+    }, 1500 );
+    
+}
+
 tarjetas.forEach((tarjeta) =>
     tarjeta.addEventListener("click", voltearTarjeta)
 );
